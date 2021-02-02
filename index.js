f75b26cb078e438afad2e60723361c5252f88540
Daniel Gogov <danielgogovmail@gmail.com>
2021-02-02 21:13:51 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('JS');
+// .accordion__element__hidden.is-active{
+//     display: block; 
+//     animation: fadeIn 3s;
+//  }
+ 
+//  @keyframes fadeIn {
+//      from { opacity: 0; }
+//      to { opacity: 1; }
+//  }
